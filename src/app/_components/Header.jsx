7b4b5e6eb42b5468f6ccb59350c6e47c7599a516
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-08-01 17:56:23 +0600

    first commit

---
+++ src/app/_components/Header.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useUser, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+const Header = () => {
+  const { user, isSignedIn } = useUser();
+  return (
+    <div className="p-5 flex justify-between items-center border-md shadow-sm">
+      <div className="flex flex-row items-center">
+        <Image src={"/chart-donut.svg"} alt="logo" width={40} height={40} />
+        <span className="text-blue-800">FinanSmart</span>
+      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-3 items-center">
+          <Link href={"/dashboard"}>
+            <Button variant="outline" className="rounded-full">
+              Dashboard
+            </Button>
+          </Link>
+          <Link href={"/sign-in"}>
+            <Button variant="primary" className="rounded-full">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
